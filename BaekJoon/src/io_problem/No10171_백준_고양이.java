8a6d76a93c56abaa88bb2768272d54f710e1f07d
DevDG <jaedark@gmail.com>
2020-04-06 22:59:18 +0900

    단계별 풀어보기 도전 1. 입출력 사칙연산 	- 고양이 10171

---
+++ BaekJoon/src/io_problem/No10171_백준_고양이.java
@@ -0,0 +1,16 @@
+package io_problem;
+
+public class No10171_백준_고양이 {
+
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer();
+		
+		// 한줄 출력은 System.out.println()을 이용한다.
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+
+	}
+
+}
