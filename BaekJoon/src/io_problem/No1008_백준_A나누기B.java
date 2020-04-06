a342b36a70e65868b8b107cd2d9f61165ba54ba0
DevDG <jaedark@gmail.com>
2020-04-06 23:37:41 +0900

    #백준 단계별 풀이 1단계 완료

---
+++ BaekJoon/src/io_problem/No1008_백준_A나누기B.java
@@ -0,0 +1,19 @@
+package io_problem;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No1008_백준_A나누기B {
+	static double A, B;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		A = Double.parseDouble(st.nextToken());
+		B = Double.parseDouble(st.nextToken());
+		
+		System.out.println(A/B);
+	}
+
+}
---
+++ BaekJoon/src/io_problem/No2588_백준_곱셈.java
@@ -0,0 +1,26 @@
+package io_problem;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class No2588_¹éÁØ_°ö¼À {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		//¹éÀÇ ÀÚ¸® ¼ö
+		int hundred = B/100;
+		//½ÊÀÇ ÀÚ¸´ ¼ö
+		int ten = (B - hundred*100) / 10;
+		//ÀÏÀÇ ÀÚ¸´ ¼ö
+		int one = (B - hundred*100 - ten*10);
+
+		System.out.println(A*one);
+		System.out.println(A*ten);
+		System.out.println(A*hundred);
+		System.out.println(A*one + ((A*ten)*10) + (A*hundred)*100);	
+	}
+
+}
